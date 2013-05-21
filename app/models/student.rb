90505fec7a425a537a82e38ec734ad3c930da588
Denk <denisse.cayetano@gmail.com>
2013-05-21 00:38:09 -0400

    fixed calculate

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,24 +1,19 @@
 class Student
-  attr_reader :matricula,:firstname, :lastname, :career, :division, :data, :courses, :credits
+  attr_reader :matricula,:firstname, :lastname, :career, :division, :courses, :credits
 
   def initialize(matricula)
     client = Savon::Client.new("https://www.academico.espol.edu.ec/Services/wsSAAC.asmx?WSDL")
     response = client.request :web, :materias_malla_aprobadas, body: { :matricula => matricula }
     raw_data = response.to_hash[:materias_malla_aprobadas_response][:materias_malla_aprobadas_result][:diffgram][:new_data_set][:v_materias_acreditadas]
     response = client.request :web, :creditos_estudiante, body: { "codigoEstudiante" => matricula, "codigoDivision" => '', "codigoCarrera" => '', "codigoEspecializacion" => '' }
-    @data = response.to_hash[:creditos_estudiante_response][:creditos_estudiante_result]
+    data = response.to_hash[:creditos_estudiante_response][:creditos_estudiante_result]
     #@data=raw_data
-    @firstname=@data[:nombres]
-    @lastname=@data[:apellidos]
-    @career=@data[:nombre_carrera]
+    @firstname=data[:nombres]
+    @lastname=data[:apellidos]
+    @career=data[:nombre_carrera]
     @matricula=matricula;
     @credits= Array.new
     @courses= Array.new
-    @data[:creditos][:tipo_credito].each do |d|
-      d[:materias_creditos][:materia].each do |c|
-        @credits << c
-      end
-    end
     raw_data.each do |d|
       if d[:promedio].to_f!=0 
         course = Course.new
@@ -33,21 +28,32 @@ class Student
         @courses << course
       end
     end
+    data[:creditos][:tipo_credito].each do |d|
+      d[:materias_creditos][:materia].each do |c|
+        @courses.collect {|x| x.credits=c[:numero_credito].to_i if x.code==c[:codigo_materia] }
+      end
+    end
   end
   def calculate_gpa
+    val=1
     grades=0
+    @credits=0
+    @division=0
     self.courses.each do |c|
-      if c.grade_average<=10 && c.grade_average>9
-        val=4 #A
-      elsif c.grade_average<=9 && c.grade_average>8
-        val=3 #B
-      elsif c.grade_average<=8 && c.grade_average>7
-        val=2 #C
-      elsif c.grade_average<=7 && c.grade_average>=6
-        val=1 #D
+      @division+=1
+      @credits+=c.credits unless c.credits.nil?
+      if c.grade_average>9
+        val=4.0
+      elsif c.grade_average>8
+        val=3.0
+      elsif c.grade_average>7
+        val=2.0
+      else
+        val=1.0
       end
-      grades+=val*4
+      grades+=(val*c.credits.to_f) unless c.credits.nil?
     end
-    return grades/courses.size
+    #@division=courses.count
+    return grades/@credits.to_f
   end
 end

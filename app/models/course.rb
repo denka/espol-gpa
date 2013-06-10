4fdcba97f8bb0273eaa88c2d796271c0d501ff97
Denk <denisse.cayetano@gmail.com>
2013-06-09 21:49:40 -0400

    update calculus based on score certificate

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,34 +1,26 @@
 class Course
   attr_accessor :id, :code, :name, :credits, :grade_average, :term, :year, :code2
   def gpa_scale
-    if self.grade_average >=9.4
+    if self.grade_average >=9
       return 4
-    elsif self.grade_average >=8.5
-      return 3.9
     elsif self.grade_average >=8
-      return 3.7
-    elsif self.grade_average >=7.5
+      return 3.9
+    elsif self.grade_average >=7
       return 3.3
-    elsif self.grade_average >7.0
-      return 3
     elsif self.grade_average >=6.5
-      return 2.7
+      return 3.0
     end
     return 2.3
   end
   def gpa_scale_letter
-    if self.grade_average >=9.4
+    if self.grade_average >=9
       return 'A+'
-    elsif self.grade_average >=8.5
-      return 'A'
     elsif self.grade_average >=8
-      return 'A-'
-    elsif self.grade_average >7.5
-      return 'B+'
+      return 'A'
     elsif self.grade_average >=7
+      return 'B+'
+    elsif self.grade_average >6.5
       return 'B'
-    elsif self.grade_average >=6.5
-      return 'B-'
     end
     return 'C'
   end
